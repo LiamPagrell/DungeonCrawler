d903d25ffd42d561a04260b480225f29e53e8813
FeliciaTorwald <113012104+FeliciaTorwald@users.noreply.github.com>
2022-11-02 13:26:01 +0100

    spawner

---
+++ Assets/Script/Spawner.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemy;
+    public int amount = 5;
+
+
+    float width, height;
+
+    void Start()
+    {
+        //Get screen size
+        width = Camera.main.orthographicSize * Camera.main.aspect;
+        height = Camera.main.orthographicSize;
+
+        //Spawn the first wave
+        SpawnWave(amount);
+    }
+
+    private void SpawnWave(int numberOfEnemies)
+    {
+        //Spawn meteors
+        for (int i = 0; i < numberOfEnemies; i++)
+        {
+            SpawnEnemy();
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Vector2 randomPosition = new Vector2();
+        randomPosition.x = Random.Range(-width, width);
+        randomPosition.y = Random.Range(-height, height);
+
+        Quaternion randomRotation = Quaternion.Euler(0, 0, Random.Range(0, 360f));
+
+        Instantiate(enemy, randomPosition, randomRotation, transform);
+    }
+}
